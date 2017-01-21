fc4f839fa5a93ed9e5c8bb25e15544be77b2b27e
CaptainAgazor <agazorMagic@gmail.com>
2017-01-21 15:02:08 +0100

    LinkedStack
    A basic implementation of a stack using Nodes.

---
+++ Stack/Test/LinkedList__Test/Test_LinkedStack.java
@@ -0,0 +1,51 @@
+package LinkedList__Test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import LinkedStack.Stack;
+
+public class Test_LinkedStack {
+	
+	private Stack<Integer> stack;
+	private final int MAX = 100;
+	
+	@Before
+	public void antes(){
+		 stack = new Stack<Integer>(); 
+	}
+	
+	@Test
+	public void testAdd() {
+		for ( int i = 0; i < MAX; i++){
+			stack.push(i);
+		}
+		assertEquals(MAX, stack.size());
+	}
+	
+	@Test
+	public void testPeek() {
+		testAdd();
+		assertEquals(new Integer(MAX-1), stack.peek());
+	}
+	
+	@Test
+	public void testRemove() {
+		testAdd();
+		for ( Integer i = MAX -1; i >= 0; i--){
+			assertEquals(i,stack.pop());
+		}
+		assertEquals(0, stack.size());
+	}
+	
+	@Test
+	public void testClear() {
+		testAdd();
+		assertEquals(MAX, stack.size());
+		stack.clear();
+		assertEquals(0, stack.size());
+	}
+	
+	
+}
---
+++ Stack/src/LinkedStack/Stack.java
@@ -0,0 +1,99 @@
+package LinkedStack;
+import java.util.NoSuchElementException;
+import java.util.Iterator;
+
+public class Stack <T> implements Iterable<T>{
+
+	private Node currentNode;
+	private int numElements;
+	
+	public Stack(){
+		this.currentNode = null;
+		this.numElements = 0;
+	}
+	
+	public int size(){
+		return this.numElements;
+	}
+	
+	public boolean isEmpty(){
+		return this.currentNode == null;
+	}
+	
+	public T push(T element){
+		
+		currentNode = new Node(element,currentNode);
+		numElements++;
+		return element;
+	}
+	
+	public T pop(){
+        if (isEmpty()) 
+        	throw new NoSuchElementException("There are not elements in the stacl to delete");
+        
+        T element = currentNode.element;
+        currentNode = currentNode.previous;
+        numElements--;
+        
+        return element;
+	}
+	
+	public T peek(){
+		return currentNode.element;
+	}
+	
+	public void clear(){
+		while ( ! isEmpty()){
+			pop();
+		}
+	}
+	
+	@Override
+	public Iterator<T> iterator() {
+		return new stackIterator();
+	}
+	
+	@Override
+	public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (T item : this)
+            s.append(item + " ");
+        return s.toString();
+    }
+	
+	private class Node{
+		
+		private T element;
+		private Node previous;
+		
+		private Node(T element,Node previous){
+			this.element = element;
+			this.previous = previous;
+		}
+	}
+	
+	private class stackIterator implements Iterator<T>{
+
+		private Node current;
+		private stackIterator(){
+			this.current = currentNode;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public T next() {
+			T element = current.element;
+			current = current.previous;
+			return element;
+		}
+		
+        public void remove(){ 
+        	throw new UnsupportedOperationException();  
+        }
+		
+	}
+}
